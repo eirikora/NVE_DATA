dcbd2fcd3a45f0e4537a1c4209d24101d3fb6a23
Eirik Y. Øra <eiyaor@nve.no>
2025-11-21 13:17:49 +0100

    Oppdatert downloadall.bat

---
+++ downloadall.bat
@@ -0,0 +1,20 @@
+python lastned_nve_dammer.py
+# python lastned_nve_delfelt.py
+# python lastned_nve_elvenett_del1.py
+# python lastned_nve_elvenett_del2.py
+# python lastned_nve_havvind.py
+python lastned_nve_ikke_utbygd_dam.py
+python lastned_nve_ikke_utbygd_inntakspunkt.py
+python lastned_nve_ikke_utbygd_magasin.py
+python lastned_nve_ikke_utbygd_vannkraftverk.py
+python lastned_nve_ikke_utbygd_vannvei.py
+# python lastned_nve_innsjøer.py
+python lastned_nve_inntakspunkt.py
+python lastned_nve_magasiner.py
+# python lastned_nve_solkraft.py
+python lastned_nve_utlopspunkt.py
+python lastned_nve_vannkraftverk.py
+python lastned_nve_vannveier.py
+# python lastned_nve_varme.py
+# python lastned_nve_vassdragregine.py
+# python lastned_nve_vindkraftverk.py
